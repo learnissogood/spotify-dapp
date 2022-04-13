b55e0a597d1483f8cf30c332dbb2a73a0768bd90
juanchi097 <juanchi097@gmail.com>
2022-04-13 15:17:46 -0300

    Contract Folder: Constructor Function

--- src/backend/contracts/MusicNFTMarketplace.sol
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -8,6 +8,36 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MusicNFTMarketplace is ERC721("DAppiFy", "FY"), Ownable {
     
     string public baseURI = "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
+    string public baseExtension = ".json";
+    address public artist;
+    uint256 public royaltyFee;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        uint256 price;
+    }
+
+    MarketItem[] public marketItems;
+
+    constructor (
+        uint256 _royaltyFee,
+        address _artist,
+        uint256[] memory _prices
+    ) payable {
+
+        require(
+            _prices.length * _royaltyFee <= msg.value,
+            "Deployer must pay royalty fee for each token listed on the marketplace"
+        );
+        royaltyFee = _royaltyFee;
+        artist = _artist;
+        for (uint8 i = 0; i < _prices.length; i++) {
+            require (_prices[i] > 0, "Price must be greater than 0");
+            _mint(address(this), i);
+            marketItems.push(MarketItem(i, payable(msg.sender), _prices[i]));
+        }
+    }
 
 
 }

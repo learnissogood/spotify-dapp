0067fb6fe5557fa02bb35d435fc5f19212e7d3cc
juanchi097 <juanchi097@gmail.com>
2022-04-15 12:31:45 -0300

    getAllUnsoldTokens, getMyTokens and _baseURI functions created

--- src/backend/contracts/MusicNFTMarketplace.sol
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -80,5 +80,36 @@ contract MusicNFTMarketplace is ERC721("DAppiFy", "FY"), Ownable {
         emit MarketItemRelisted(_tokenId, msg.sender, _price);
     }
 
+    /* Fetches all the tokens currently listed for sale */
+    function getAllUnsoldTokens() external view returns (MarketItem[] memory) {
+        uint256 unsoldCount = balanceOf(address(this));
+        MarketItem[] memory tokens = new MarketItem[](unsoldCount);
+        uint256 currentIndex;
+        for (uint256 i = 0; i < marketItems.length; i++) {
+            if (marketItems[i].seller != address(0)) {
+                tokens[currentIndex] = marketItems[i];
+                currentIndex++;
+            }
+        }
+        return tokens;
+    }
 
+    /* Fetches all the tokens owned by the user */
+    function getMyTokens() external view returns (MarketItem[] memory) {
+        uint256 myTokenCount = balanceOf(msg.sender);
+        MarketItem[] memory tokens = new MarketItem[](myTokenCount);
+        uint256 currentIndex;
+        for (uint256 i = 0; i < marketItems.length; i++) {
+            if (ownerOf(i) == msg.sender) {
+                tokens[currentIndex] = marketItems[i];
+                currentIndex++;
+            }
+        }
+        return tokens;
+    }
+
+    /* Internal function that gets the baseURI initialized in the constructor */
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
 }
